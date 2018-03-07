8a77e939ec5cf0dfe8e2015db4df91b92aec60ca
root <allimut@gmail.com>
2018-03-07 14:46:10 +0000

    update

---
+++ bin/file1.py
@@ -0,0 +1,7 @@
+#!/bin/python
+
+xmen_file = open('filename.txt', 'a')
+
+xmen_file.write("\ngrapes\n")
+
+xmen_file.close()
---
+++ bin/fileadd.py
@@ -0,0 +1,8 @@
+#!/bin/python
+
+xmen_file =  open('filename.txt', 'r')
+
+for line in xmen_file:
+	print(line)
+
+xmen_file.close()
